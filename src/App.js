ca2a2c3d3f518dc3bcda564174c2312348faa0da
gfbollinger <gfbollinger@gmail.com>
2022-09-10 10:17:02 -0300

    Improve converter functionality and add flags

--- src/App.js
+++ src/App.js
@@ -1,23 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState, useRef } from "react";
+import "./App.css";
+import CurrencyInput from "./components/CurrencyInput";
+
+var myHeaders = new Headers();
+myHeaders.append("apikey", process.env.REACT_APP_FIXER_IO_APIKEY);
+
+var requestOptions = {
+  method: "GET",
+  redirect: "follow",
+  headers: myHeaders,
+};
 
 function App() {
+  const [currencyA, setCurrencyA] = useState("USD");
+  const [currencyB, setCurrencyB] = useState("EUR");
+  const [amountA, setAmountA] = useState("");
+  const [amountB, setAmountB] = useState("");
+  const [currencies, setCurrencies] = useState([]);
+  const [currencyRates, setCurrencyRates] = useState([]);
+
+  let currenciesDate = useRef("");
+
+  useEffect(() => {
+    fetch(`https://api.apilayer.com/exchangerates_data/latest?base=USD&apikey=${process.env.REACT_APP_FIXER_IO_APIKEY}`, requestOptions)
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result);
+        setCurrencies(Object.keys(result.rates));
+        setCurrencyRates(result.rates);
+        currenciesDate.current = result.date;
+      })
+      .catch((error) => console.log("error", error));
+  }, []);
+
+  useEffect(() => {
+    if (!currencyRates) {
+      function init() {
+        handleAmountChange(1, "a");
+      }
+      init();
+    }
+  }, [currencyRates]);
+
+  const handleAmountChange = (amount, id) => {
+    if (id === "a") {
+      setAmountB(((amount * currencyRates[currencyB]) / currencyRates[currencyA]).toFixed(4));
+      setAmountA(amount);
+    }
+    if (id === "b") {
+      setAmountA(((amount * currencyRates[currencyA]) / currencyRates[currencyB]).toFixed(4));
+      setAmountB(amount);
+    }
+  };
+
+  const handleCurrencyChange = (currency, id) => {
+    console.log(currency, id);
+    if (id === "a") {
+      setAmountB(((amountA * currencyRates[currencyB]) / currencyRates[currency]).toFixed(4));
+      setCurrencyA(currency);
+    }
+    if (id === "b") {
+      setAmountB(((amountA * currencyRates[currency]) / currencyRates[currencyA]).toFixed(4));
+      setCurrencyB(currency);
+    }
+  };
+
+  const handleRecerseCurrencies = () => {
+    setCurrencyA(currencyB);
+    setCurrencyB(currencyA);
+    setAmountB(() => {
+      return ((amountA * currencyRates[currencyB]) / currencyRates[currencyA]).toFixed(4);
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <CurrencyInput
+        currencies={currencies}
+        currency={currencyA}
+        onCurrencyChange={handleCurrencyChange}
+        amount={amountA}
+        onAmountChange={handleAmountChange}
+        inputId="a"
+      />
+      =
+      <CurrencyInput
+        currencies={currencies}
+        currency={currencyB}
+        onCurrencyChange={handleCurrencyChange}
+        amount={amountB}
+        onAmountChange={handleAmountChange}
+        inputId="b"
+      />
+      <button onClick={handleRecerseCurrencies}>Reverse Currencies</button>
+      {currenciesDate.current && <p>Last updated: {currenciesDate.current}</p>}
     </div>
   );
 }
--- src/components/CurrencyInput.js
+++ src/components/CurrencyInput.js
@@ -1,31 +1,47 @@
-const CurrencyInput = ({ inputId, currencies, currency, onCurrencyChange, amount, onAmountChange, label }) => {
+import "currency-flags/dist/currency-flags.css";
+import Select from "react-select";
+
+import { components } from "react-select";
+const { Option } = components;
+
+const OptionComponent = (props) => {
+  return (
+    <Option {...props}>
+      <span className={`currency-flag currency-flag-${props.data.value.toLowerCase()}`} /> {props.data.label}
+    </Option>
+  );
+};
+
+const CurrencyInput = ({ inputId, currencies, currency, onCurrencyChange, amount, onAmountChange }) => {
+  let options = [];
+  currencies.map((item) => {
+    return options.push({ value: item, label: item });
+  });
+
   return (
     <div>
-      <input
-        type="number"
-        value={amount}
-        onChange={(e) => onAmountChange(e.target.value, inputId)}
-      />
-
-      <select
-        onChange={(e) => onCurrencyChange(e.target.value, inputId)}
+      <input type="number" value={amount} onChange={(e) => onAmountChange(e.target.value, inputId)} />
+      <Select
+        onChange={onCurrencyChange}
         value={currency}
-      >
-        {
-          currencies && currencies.map( item => {
+        options={options}
+        components={{ Option: OptionComponent }}
+        styles={{ control: (styles) => ({ ...styles, borderRadius: "20px" }) }}
+      />
+      {/* <select onChange={(e) => onCurrencyChange(e.target.value, inputId)} value={currency}>
+        {currencies &&
+          currencies.map((item) => {
             return (
-              <option
-                value={item}
-                key={item}
-              >
-                {item}
-              </option>
-            )
-          })
-        }
-      </select>
+              <>
+                <option className="currency-flag currency-flag-usd" value={item} key={item}>
+                  {item}
+                </option>
+              </>
+            );
+          })}
+      </select> */}
     </div>
-  )
-}
+  );
+};
 
-export default CurrencyInput
+export default CurrencyInput;
